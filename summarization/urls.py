483ed7b819cfddb303f4bc2cc5473939bf728f89
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-02-11 20:12:34 +0200

    added date parameters to urls

--- summarization/urls.py
+++ summarization/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import summariztion_passed_hourd_average, summariztion_hourly_average, summariztion_daily_average, summariztion_monthly_average, summariztion_yearly_average
 
 urlpatterns = [
-	path('<int:hour>/passed-average', summariztion_passed_hourd_average, name='passed_hourd_average'),
-	path('<int:hour>/average', summariztion_hourly_average, name='hourly_average'),
-	path('<int:year>/<int:month>/average', summariztion_monthly_average, name='monthly_average'),
-	path('<int:year>/<int:month>/<int:day>/average', summariztion_daily_average, name='daily_average'),
-	path('<int:year>/average-year', summariztion_yearly_average, name='yearly_average'),
+	path('api/<int:hour>/passed-average', summariztion_passed_hourd_average, name='passed_hourd_average'),
+	path('api/<int:hour>/average', summariztion_hourly_average, name='hourly_average'),
+	path('api/<int:year>/<int:month>/average', summariztion_monthly_average, name='monthly_average'),
+	path('api/<int:year>/<int:month>/<int:day>/average', summariztion_daily_average, name='daily_average'),
+	path('api/<int:year>/average-year', summariztion_yearly_average, name='yearly_average'),
 ]
