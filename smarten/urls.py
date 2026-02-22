f52e182998afcc9db5c9dcefee11c3ccdcb86367
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-02-22 16:01:35 +0200

    added goals app 9

--- smarten/urls.py
+++ smarten/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('authentication/', include('authentication.urls')),
+    path('goals/', include('goals.urls')),
 ]
