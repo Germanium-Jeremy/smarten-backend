6330fab7b45020cb69b41fa38766a69eda845e2a
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-02-24 14:42:09 +0200

    added the account deletion variable

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
-from .views import authentication_register, authentication_verify_email
+from .views import authentication_register, authentication_verify_email, authentication_login
 
 urlpatterns = [
      path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
      path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
      path('api/register/', authentication_register, name='register'),
+     path('api/login/', authentication_login, name='login'),
      path('api/verify-email/', authentication_verify_email, name='verify_email'),
 ]
