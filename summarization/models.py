c7dbd1449c4eee0bbcb66125a74277a83b6e6a7b
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-02-25 21:21:34 +0200

    implemented the functions needed

--- summarization/models.py
+++ summarization/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from sensors.models import UserSensors
 
 # Create your models here.
+class SensorData(models.Model):
+    sensor = models.ForeignKey(UserSensors, on_delete=models.CASCADE, related_name='data')
+    flow_rate = models.FloatField()
+    volume = models.FloatField()
+    status = models.CharField(max_length=10)
+    timestamp = models.DateTimeField(auto_now_add=True)
