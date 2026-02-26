b04d7cb11474f78beb79d28dd946a19775962e86
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-02-26 12:08:55 +0200

    finished summarization data

--- summarization/urls.py
+++ summarization/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 	path('<int:hour>/average', summariztion_hourly_average, name='hourly_average'),
 	path('<int:year>/<int:month>/average', summariztion_monthly_average, name='monthly_average'),
 	path('<int:year>/<int:month>/<int:day>/average', summariztion_daily_average, name='daily_average'),
-	path('<int:year>/average', summariztion_yearly_average, name='yearly_average'),
+	path('<int:year>/average-year', summariztion_yearly_average, name='yearly_average'),
 ]
