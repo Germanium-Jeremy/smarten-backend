01de2a37618cb03a781edf6ccb7e78862e316364
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-03-02 08:39:32 +0200

    added templates for email verification and default profiles 2

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
-from .views import authentication_register
+from .views import authentication_register, authentication_verify_email
 
 urlpatterns = [
      path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
      path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
      path('api/register/', authentication_register, name='register'),
+     path('api/verify-email/', authentication_verify_email, name='verify_email'),
 ]
