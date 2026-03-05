d4b4a0a54b43f496c576bc6bec253e00855b9c92
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-03-05 10:03:33 +0200

    renamed user to usermodel 6

--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from authentication.models import UserModel as User
 
 # Create your models here.
+class Goals(models.Model):
+     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals')
+     daily_goals = models.FloatField(default=0.0)
+     monthly_goals = models.FloatField(default=0.0)
+     conservation_goal = models.JSONField(default=list)
+     cost_goal = models.FloatField(default=0.0)
+
+     def to_dict(self):
+          return {
+               'daily_goals': self.daily_goals,
+               'monthly_goals': self.monthly_goals,
+               'conservation_goal': self.conservation_goal,
+               'cost_goal': self.cost_goal,
+          }
