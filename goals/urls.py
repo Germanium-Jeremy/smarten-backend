031786643872188fb7da35757b63d841a2e7f972
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-03-05 10:03:17 +0200

    added create goals

--- goals/urls.py
+++ goals/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main_view
+from .views import goals_get_goals, goals_set_goals
 
 urlpatterns = [
-    
+    path('api/get-goals/', goals_get_goals, name='get_goals'),
+    path('api/set-goals/', goals_set_goals, name='set_goals'),
 ]
