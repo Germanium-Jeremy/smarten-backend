12cae1c6b9c437e401de490c91dfc390a48e2804
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-03-07 20:24:56 +0200

    updated to send html rendered email instead of plain text

--- smarten/utils.py
+++ smarten/utils.py
@@ -3,11 +3,12 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 def send_templated_email(subject, recipient, template_name, context):
-     message = render_to_string(template_name, context)
+     html_content = render_to_string(template_name, context)
      send_mail(
           subject=subject,
-          message=message,
+          message='', # Plain text fallback
           from_email=os.getenv('DEFAULT_FROM_EMAIL'),
           recipient_list=[recipient],
           fail_silently=False,
+          html_message=html_content
      )
