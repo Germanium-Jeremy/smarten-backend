ddf44e9ddd80ab62faf4221db7493dac72c48102
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-03-09 20:28:41 +0200

    added user authenticated views from authentication

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import me
+from .views import users_me, users_update_profile, users_profile_change
 
 urlpatterns = [
-     path('api/me/', me, name='me'),
+     path('api/me/', users_me, name='users_me'),
+     path('api/update_profile', users_update_profile, name='users_update_profile'),
+     path('api/profile_change', users_profile_change, name='users_profile_change'),
 ]
