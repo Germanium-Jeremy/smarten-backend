6030361b7773da7b88eb010564145503fdacae58
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-03-10 20:23:32 +0200

    added websockets 2

---
+++ sensors/routing.py
@@ -0,0 +1,7 @@
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/sensorData/(?P<mac_address>[^/]+)/$', consumers.SensorDataConsumer.as_asgi()),
+    re_path(r'ws/notifications/(?P<user_id>[^/]+)/$', consumers.NotificationConsumer.as_asgi()),
+]
