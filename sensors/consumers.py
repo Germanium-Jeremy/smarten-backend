e6430da0eccc9ebbb0b4ad82a1c30c818450a77f
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-03-10 20:23:23 +0200

    added websockets

---
+++ sensors/consumers.py
@@ -0,0 +1,53 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class SensorDataConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.mac_address = self.scope['url_route']['kwargs']['mac_address']
+        self.group_name = f'sensor_{self.mac_address}'
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    # Receive message from room group
+    async def sensor_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'data': message
+        }))
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_id = self.scope['url_route']['kwargs']['user_id']
+        self.group_name = f'notify_{self.user_id}'
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    async def notify_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps(message))
