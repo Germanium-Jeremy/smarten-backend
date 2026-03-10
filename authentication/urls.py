610a989364635ac15a8aa9f8f30fac185eab4236
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-03-10 20:21:25 +0200

    removed the user authenticated views to users 2

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
-from .views import authentication_register, authentication_verify_email, authentication_login, authentication_update_profile, authentication_profile_change
+from .views import authentication_register, authentication_verify_email, authentication_login
 
 urlpatterns = [
      path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -8,6 +8,4 @@ urlpatterns = [
      path('api/register/', authentication_register, name='register'),
      path('api/login/', authentication_login, name='login'),
      path('api/verify-email/', authentication_verify_email, name='verify_email'),
-     path('update_profile', authentication_update_profile, name='update_profile'),
-     path('profile_change', authentication_profile_change, name='profile_change'),
 ]
